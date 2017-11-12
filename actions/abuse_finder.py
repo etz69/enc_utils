ddf9e61c30381c96653e54ed6faf7cded8aecf17
etz69 <etz69.dev@gmail.com>
2017-11-12 17:24:06 +0400

    Fix

--- actions/abuse_finder.py
+++ actions/abuse_finder.py
@@ -11,12 +11,11 @@ class AbuseFinder(Action):
         self.logger.info('Abuse Finder called')
         _abuse_contact = ""
 
-
         try:
             result = abuse_finder.domain_abuse(tld)
             _abuse_contact = str(result['abuse'][0])
         except Exception as e:
-            self.logger.error('Abuse finder ended with error')
+            self.logger.error('Abuse finder ended with error %s',e)
             return False, _abuse_contact
 
         return True, _abuse_contact
